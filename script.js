3f363c68433333928fc77cdda658b5c85e56fbbf
kumbek <113044669+kumbek@users.noreply.github.com>
2022-09-20 23:13:53 +0600

    Add files via upload
    home work 2 js.

---
+++ script.js
@@ -0,0 +1,11 @@
+//ДЗ-2
+
+let trafficlight=prompt("выбрать цвет светофора");
+
+if (trafficlight === "красный"){
+    console.log("Остановись")
+}else if(trafficlight === "зеленый"){
+    console.log("Можно идти")
+}else if(trafficlight==="желтый") {
+    console.log("Подожди")
+}
